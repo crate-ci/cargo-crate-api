bf6648bd345fd52e94220fe27af1c1032dd4b3ab
Ed Page <eopage@gmail.com>
2021-12-14 19:44:34 -0600

    feat(cli): Process cargo flags

--- crates/cargo-api/src/main.rs
+++ crates/cargo-api/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::Write;
 
+use proc_exit::WithCodeResultExt;
 use structopt::StructOpt;
 
 mod args;
@@ -28,6 +29,16 @@ fn run() -> proc_exit::ExitResult {
     let colored_stderr = concolor_control::get(concolor_control::Stream::Stderr).ansi_color();
 
     log::init_logging(args.verbose.clone(), colored_stderr);
+
+    let metadata = args
+        .manifest
+        .metadata()
+        .exec()
+        .with_code(proc_exit::Code::CONFIG_ERR)?;
+    let (selected, _) = args.workspace.partition_packages(&metadata);
+    for selected in selected {
+        dbg!(selected);
+    }
 
     Ok(())
 }

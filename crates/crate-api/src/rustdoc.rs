4a171dc0971905c76db086d10e01758d602d53dc
Ed Page <eopage@gmail.com>
2021-12-17 15:38:41 -0600

    fix(api): Ensure we process each item just once

--- crates/crate-api/src/rustdoc.rs
+++ crates/crate-api/src/rustdoc.rs
@@ -135,9 +135,13 @@ pub fn parse_raw(raw: &str, manifest_path: &std::path::Path) -> Result<crate::Ap
 
 #[derive(Default)]
 struct RustDocParser {
+    unprocessed: VecDeque<(Option<crate::PathId>, rustdoc_json_types_fork::Id)>,
+    deferred_imports: VecDeque<rustdoc_json_types_fork::Id>,
+
     api: crate::Api,
-    crate_ids: HashMap<u32, crate::CrateId>,
-    path_ids: HashMap<rustdoc_json_types_fork::Id, crate::PathId>,
+    crate_ids: HashMap<u32, Option<crate::CrateId>>,
+    path_ids: HashMap<rustdoc_json_types_fork::Id, Option<crate::PathId>>,
+    item_ids: HashMap<rustdoc_json_types_fork::Id, Option<crate::ItemId>>,
 }
 
 impl RustDocParser {
@@ -161,14 +165,11 @@ impl RustDocParser {
             )
         })?;
 
-        let mut deferred_imports = VecDeque::new();
-
-        let mut unprocessed = VecDeque::new();
-        unprocessed.push_back((None, &raw.root));
-        while let Some((parent_path_id, raw_item_id)) = unprocessed.pop_front() {
+        self.unprocessed.push_back((None, raw.root.clone()));
+        while let Some((parent_path_id, raw_item_id)) = self.unprocessed.pop_front() {
             let raw_item = raw
                 .index
-                .get(raw_item_id)
+                .get(&raw_item_id)
                 .expect("all item ids are in `index`");
 
             let crate_id = self._parse_crate(&raw, raw_item.crate_id);
@@ -177,43 +178,10 @@ impl RustDocParser {
                 ._parse_path(&raw, parent_path_id, &raw_item_id, crate_id)
                 .or(parent_path_id);
 
-            match &raw_item.inner {
-                rustdoc_json_types_fork::ItemEnum::Module(module) => {
-                    unprocessed.extend(module.items.iter().map(move |i| (path_id, i)));
-                }
-                rustdoc_json_types_fork::ItemEnum::Import(_) => {
-                    deferred_imports.push_back(raw_item_id);
-                }
-                rustdoc_json_types_fork::ItemEnum::Trait(trait_) => {
-                    unprocessed.extend(trait_.items.iter().map(move |i| (path_id, i)));
-                }
-                rustdoc_json_types_fork::ItemEnum::Impl(impl_) => {
-                    unprocessed.extend(impl_.items.iter().map(move |i| (path_id, i)));
-                }
-                _ => {
-                    assert_ne!(self.api.root_id, None, "Module should be root");
-                    let mut item = crate::Item::new();
-                    item.crate_id = crate_id;
-                    item.name = raw_item.name.clone();
-                    item.span = raw_item.span.clone().map(|raw_span| crate::Span {
-                        filename: raw_span.filename,
-                        begin: raw_span.begin,
-                        end: raw_span.end,
-                    });
-                    let item_id = self.api.items.push(item);
-
-                    if let Some(path_id) = path_id {
-                        self.api
-                            .paths
-                            .get_mut(path_id)
-                            .expect("path_id to always be valid")
-                            .item_id = Some(item_id);
-                    }
-                }
-            }
+            self._parse_item(&raw, &raw_item_id, path_id, crate_id);
         }
 
-        for raw_item_id in deferred_imports {
+        for raw_item_id in &self.deferred_imports {
             let raw_item = raw
                 .index
                 .get(raw_item_id)
@@ -223,7 +191,7 @@ impl RustDocParser {
                 _ => unreachable!("deferred_imports only contains imports"),
             };
             let raw_target_id = import.id.as_ref().unwrap();
-            let target_path_id = *self.path_ids.get(raw_target_id).unwrap();
+            let target_path_id = self.path_ids.get(raw_target_id).unwrap().unwrap();
             let target_path = self
                 .api
                 .paths
@@ -231,7 +199,7 @@ impl RustDocParser {
                 .expect("path_id to always be valid")
                 .clone();
 
-            let path_id = *self.path_ids.get(raw_item_id).unwrap();
+            let path_id = self.path_ids.get(raw_item_id).unwrap().unwrap();
             let path = self
                 .api
                 .paths
@@ -249,16 +217,20 @@ impl RustDocParser {
         raw: &rustdoc_json_types_fork::Crate,
         raw_crate_id: u32,
     ) -> Option<crate::CrateId> {
-        (raw_crate_id != 0).then(|| {
-            *self.crate_ids.entry(raw_crate_id).or_insert_with(|| {
-                let raw_crate = raw
-                    .external_crates
-                    .get(&raw_crate_id)
-                    .expect("all crate ids are in `external_crates`");
-                let crate_ = crate::Crate::new(&raw_crate.name);
-                self.api.crates.push(crate_)
-            })
-        })
+        if let Some(crate_id) = self.crate_ids.get(&raw_crate_id) {
+            return *crate_id;
+        }
+
+        let crate_id = (raw_crate_id != 0).then(|| {
+            let raw_crate = raw
+                .external_crates
+                .get(&raw_crate_id)
+                .expect("all crate ids are in `external_crates`");
+            let crate_ = crate::Crate::new(&raw_crate.name);
+            self.api.crates.push(crate_)
+        });
+        self.crate_ids.insert(raw_crate_id.clone(), crate_id);
+        crate_id
     }
 
     fn _parse_path(
@@ -268,33 +240,99 @@ impl RustDocParser {
         raw_item_id: &rustdoc_json_types_fork::Id,
         crate_id: Option<crate::CrateId>,
     ) -> Option<crate::PathId> {
-        raw.paths.get(raw_item_id).map(|raw_path| {
-            *self.path_ids.entry(raw_item_id.clone()).or_insert_with(|| {
-                let raw_item = raw
-                    .index
-                    .get(raw_item_id)
-                    .expect("all item ids are in `index`");
+        if let Some(path_id) = self.path_ids.get(&raw_item_id) {
+            return *path_id;
+        }
 
-                let mut path = crate::Path::new(raw_path.path.join("::"));
-                path.crate_id = crate_id;
-                path.span = raw_item.span.clone().map(|raw_span| crate::Span {
+        let path_id = raw.paths.get(raw_item_id).map(|raw_path| {
+            let raw_item = raw
+                .index
+                .get(raw_item_id)
+                .expect("all item ids are in `index`");
+
+            let mut path = crate::Path::new(raw_path.path.join("::"));
+            path.crate_id = crate_id;
+            path.span = raw_item.span.clone().map(|raw_span| crate::Span {
+                filename: raw_span.filename,
+                begin: raw_span.begin,
+                end: raw_span.end,
+            });
+            let path_id = self.api.paths.push(path);
+
+            if let Some(parent_path_id) = parent_path_id {
+                self.api
+                    .paths
+                    .get_mut(parent_path_id)
+                    .expect("parent_path_id to always be valid")
+                    .children
+                    .push(path_id);
+            }
+            self.api.root_id.get_or_insert(path_id);
+            path_id
+        });
+        self.path_ids.insert(raw_item_id.clone(), path_id);
+        path_id
+    }
+
+    fn _parse_item(
+        &mut self,
+        raw: &rustdoc_json_types_fork::Crate,
+        raw_item_id: &rustdoc_json_types_fork::Id,
+        path_id: Option<crate::PathId>,
+        crate_id: Option<crate::CrateId>,
+    ) -> Option<crate::ItemId> {
+        if let Some(item_id) = self.item_ids.get(&raw_item_id) {
+            return *item_id;
+        }
+
+        let raw_item = raw
+            .index
+            .get(raw_item_id)
+            .expect("all item ids are in `index`");
+
+        let item_id = match &raw_item.inner {
+            rustdoc_json_types_fork::ItemEnum::Module(module) => {
+                self.unprocessed
+                    .extend(module.items.iter().map(move |i| (path_id, i.clone())));
+                None
+            }
+            rustdoc_json_types_fork::ItemEnum::Import(_) => {
+                self.deferred_imports.push_back(raw_item_id.clone());
+                None
+            }
+            rustdoc_json_types_fork::ItemEnum::Trait(trait_) => {
+                self.unprocessed
+                    .extend(trait_.items.iter().map(move |i| (path_id, i.clone())));
+                None
+            }
+            rustdoc_json_types_fork::ItemEnum::Impl(impl_) => {
+                self.unprocessed
+                    .extend(impl_.items.iter().map(move |i| (path_id, i.clone())));
+                None
+            }
+            _ => {
+                assert_ne!(self.api.root_id, None, "Module should be root");
+                let mut item = crate::Item::new();
+                item.crate_id = crate_id;
+                item.name = raw_item.name.clone();
+                item.span = raw_item.span.clone().map(|raw_span| crate::Span {
                     filename: raw_span.filename,
                     begin: raw_span.begin,
                     end: raw_span.end,
                 });
-                let path_id = self.api.paths.push(path);
+                let item_id = self.api.items.push(item);
 
-                if let Some(parent_path_id) = parent_path_id {
+                if let Some(path_id) = path_id {
                     self.api
                         .paths
-                        .get_mut(parent_path_id)
-                        .expect("parent_path_id to always be valid")
-                        .children
-                        .push(path_id);
+                        .get_mut(path_id)
+                        .expect("path_id to always be valid")
+                        .item_id = Some(item_id);
                 }
-                self.api.root_id.get_or_insert(path_id);
-                path_id
-            })
-        })
+                Some(item_id)
+            }
+        };
+        self.item_ids.insert(raw_item_id.clone(), item_id);
+        item_id
     }
 }

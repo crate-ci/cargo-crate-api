dec6cc37ec7f2653d901c4c7540b656cdd267791
Ed Page <eopage@gmail.com>
2021-12-20 09:36:46 -0600

    test(fixture): Handle rename corner case

--- fixtures/dep_upgrade/old/src/lib.rs
+++ fixtures/dep_upgrade/old/src/lib.rs
@@ -1,11 +1,11 @@
-pub use dep_upgrade_dep::Used;
+pub use dep::Used;
 
-pub fn in_func(_var: dep_upgrade_dep::InFunc) {}
+pub fn in_func(_var: dep::InFunc) {}
 
 pub struct ConvertTo;
 
-impl From<dep_upgrade_dep::ConvertFrom> for ConvertTo {
-    fn from(_other: dep_upgrade_dep::ConvertFrom) -> ConvertTo {
+impl From<dep::ConvertFrom> for ConvertTo {
+    fn from(_other: dep::ConvertFrom) -> ConvertTo {
         ConvertTo
     }
 }
